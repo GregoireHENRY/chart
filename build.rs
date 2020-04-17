e5009c10cf38ab47b3db6a73964d18e85cd8e39e
GregoireHENRY <gregoire.henry@ipsa.fr>
2020-04-17 23:23:39 +0200

    static test 004

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main()
 {
-    //println!("cargo::rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
-    //println!("cargo::rustc-link-lib=static=SDL2");
-    println!("cargo:rustc-link-search=dll");
+    println!("cargo::rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
+    println!("cargo::rustc-link-lib=static=SDL2-2.0");
 }
