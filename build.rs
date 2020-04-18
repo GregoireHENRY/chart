8f6d610670206719960b2e792f3dd178c0c233dd
GregoireHENRY <gregoire.henry@ipsa.fr>
2020-04-18 13:25:35 +0200

    dynamic test 001

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main()
-{
-    #[cfg(feature="static")]
-    {
-        //println!("cargo::rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
-        println!("cargo::rustc-link-lib=static=SDL2");
-    }
-}
