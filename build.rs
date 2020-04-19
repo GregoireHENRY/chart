ebef2962971e508f8d7d6f2788478a0846369eba
GregoireHENRY <gregoire.henry@ipsa.fr>
2020-04-19 15:05:41 +0200

    Update

--- build.rs
+++ build.rs
@@ -1,7 +1,14 @@
-use std::env::var;
-
 fn main()
 {
-    let relative = var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search={}/usr/dll", relative);
+    let root = env!("CARGO_MANIFEST_DIR");
+    println!("cargo:rustc-env=LD_LIBRARY_PATH={}/usr/dll", root);
+    println!("cargo:rustc-link-search={}/usr/dll", root);
+    println!("cargo:rustc-link-lib=SDL2");
 }
+
+/*
+SDL2
+SDL2_ttf
+freetype-6
+zlib1
+*/
